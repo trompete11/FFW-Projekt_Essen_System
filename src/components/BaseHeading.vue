f4737cd8c05c0ceb634ac8a3ace82e4bee0c714c
Lucas Höttler <lucas.hoettler@gmail.com>
2024-01-06 23:42:10 +0100

    added heading

---
+++ src/components/BaseHeading.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="heading-logo">
+    <div class="col">
+      <slot name="logo"></slot>
+    </div>
+  </div>
+  <div class="col">
+    <h2 class="header"><slot name="heading"></slot></h2>
+    <slot name="subheading"></slot>
+  </div>
+  <div class="heading-uhr">
+    <div class="col">
+      <h3 class="header"><slot name="uhr"></slot></h3>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.heading-logo {
+  height: 100%;
+  width: fit-content;
+  margin-left: 1em;
+}
+
+.heading-uhr {
+  text-align: right;
+  width: fit-content;
+  margin-right: 1em;
+}
+</style>
