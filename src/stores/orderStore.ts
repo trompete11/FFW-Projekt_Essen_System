b1a718fc0ff1b20fdac6236f41b7dc1b897f75cb
Lucas Höttler <lucas.hoettler@gmail.com>
2024-01-07 22:59:17 +0100

    added basic orderStore

--- src/stores/orderStore.ts
+++ src/stores/orderStore.ts
@@ -1,36 +1,41 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
+import { type Order, type OrderItem } from '@/assets/interfaces'
 
-export const useCounterStore = defineStore('counter', () => {
+export const useOrderStore = defineStore('orderStore', () => {
   const id = ref(0)
-  const orders = ref([])  // @todo: add type
+  const orders = ref([] as Order[])
 
   const getOrders = computed(() => orders.value)
-  const getFiltertOrders = computed((filter: String) => orders.value)
+  const getFilterdOrders = computed((filter: String) => orders.value) // @todo: implementation
 
-  function addOrder(order:any) {  // @todo: update type
+  function addOrder(order_items:OrderItem[]) {
     id.value++
-    order.value.append({id, time_in: Date(), time_done: null, time_gone: null, order})
+    orders.value.push({id: id.value, time_in: Date(), time_done: null, time_gone: null, order_items})
     return id;
   }
 
-  function doneOrder(id:number, done:boolean = true){
+  function doneOrder(orderId:number, done:boolean = true){
     if(done){
-      orders.value[id].time_done = Date()
+      orders.value[orderId].time_done = Date()
     }
     else{
-      orders.value[id].time_done = null
+      orders.value[orderId].time_done = null
     }
   }
 
-  function goneOrder(id:number, gone:boolean = true){
+  function goneOrder(orderId:number, gone:boolean = true){
     if(gone){
-      orders.value[id].time_gone = Date()
+      orders.value[orderId].time_gone = Date()
     }
     else{
-      orders.value[id].time_gone = null
+      orders.value[orderId].time_gone = null
     }
   }
 
-  return { count, doubleCount, increment }
+  function stornoOrder(orderId:number){
+    orders.value.splice(orderId, 1)
+  }
+
+  return { id, orders, getOrders, getFilterdOrders, addOrder, doneOrder, goneOrder, stornoOrder }
 })
