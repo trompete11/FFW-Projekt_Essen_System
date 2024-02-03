7ff32e3074e2b59fbbf74e2a71d4ba9ed21888d8
Lucas Höttler <lucas.hoettler@gmail.com>
2024-02-03 13:46:19 +0100

    bugfix new Date

--- src/stores/orderStore.ts
+++ src/stores/orderStore.ts
@@ -11,13 +11,13 @@ export const useOrderStore = defineStore('orderStore', () => {
 
   function addOrder(order_items:OrderItem[]) {
     id.value++
-    orders.value.push({id: id.value, time_in: Date(), time_done: null, time_gone: null, order_items})
+    orders.value.push({id: id.value, time_in: new Date(), time_done: null, time_gone: null, order_items})
     return id;
   }
 
   function doneOrder(orderId:number, done:boolean = true){
     if(done){
-      orders.value[orderId].time_done = Date()
+      orders.value[orderId].time_done = new Date()
     }
     else{
       orders.value[orderId].time_done = null
@@ -26,7 +26,7 @@ export const useOrderStore = defineStore('orderStore', () => {
 
   function goneOrder(orderId:number, gone:boolean = true){
     if(gone){
-      orders.value[orderId].time_gone = Date()
+      orders.value[orderId].time_gone = new Date()
     }
     else{
       orders.value[orderId].time_gone = null
