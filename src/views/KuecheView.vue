d620dbc0797c5e868c3e6f6abe10418738fddc94
Sterio-0x1111 <xxsterioxx2@gmail.com>
2024-02-28 21:25:30 +0100

    fix merge

--- src/views/KuecheView.vue
+++ src/views/KuecheView.vue
@@ -33,74 +33,6 @@ const filter = ref('open')
 const orderStore = useOrderStore()
 
 const addData = () => {
-  const exampleOrderItem1 = {
-    count: 2,
-    price_sum: 25.99,
-    comment: "No onions",
-    item: { id: 1, name: "Burger", price: 12.99 },
-    extras: [{ id: 2, name: "Cheese", price: 1.99 }, { id: 3, name: "Chilli", price: 1.99 }]
-  };
-
-  const exampleOrderItem2 = {
-    count: 3,
-    price_sum: 8.49,
-    comment: null,
-    item: { id: 3, name: "Pizza", price: 8.49 },
-    extras: [{ id: 2, name: "Chilli", price: 1.99 }, { id: 3, name: "Onions", price: 1.99 }, { id: 3, name: "Thunfish", price: 1.99 }]
-  };
-
-  const exampleOrderItem3 = {
-    count: 1,
-    price_sum: 15.99,
-    comment: null,
-    item: { id: 2, name: "Salad", price: 15.99 },
-    extras: [{ id: 4, name: "Chicken", price: 3.99 }, { id: 5, name: "Dressing", price: 0.99 }]
-  };
-
-  const exampleOrderItem4 = {
-    count: 4,
-    price_sum: 30.99,
-    comment: "Extra cheese",
-    item: { id: 4, name: "Pasta", price: 7.49 },
-    extras: [{ id: 6, name: "Parmesan", price: 2.49 }, { id: 7, name: "Basil", price: 1.49 }]
-  };
-
-  orderStore.addOrder([exampleOrderItem1, exampleOrderItem2, exampleOrderItem3, exampleOrderItem4]);
-
-  const dlongOrderItems = [
-    {
-      count: 2,
-      price_sum: 25.99,
-      comment: "No onions",
-      item: { id: 1, name: "Burger", price: 12.99 },
-      extras: [{ id: 2, name: "Cheese", price: 1.99 }, { id: 3, name: "Chilli", price: 1.99 }]
-    },
-    {
-      count: 3,
-      price_sum: 8.49,
-      comment: null,
-      item: { id: 3, name: "Pizza", price: 8.49 },
-      extras: [{ id: 2, name: "Chilli", price: 1.99 }, { id: 3, name: "Onions", price: 1.99 }, { id: 3, name: "Thunfish", price: 1.99 }]
-    },
-    {
-      count: 1,
-      price_sum: 15.99,
-      comment: null,
-      item: { id: 2, name: "Salad", price: 15.99 },
-      extras: [{ id: 4, name: "Chicken", price: 3.99 }, { id: 5, name: "Dressing", price: 0.99 }]
-    },
-    {
-      count: 4,
-      price_sum: 30.99,
-      comment: "Extra cheese",
-      item: { id: 4, name: "Pasta", price: 7.49 },
-      extras: [{ id: 6, name: "Parmesan", price: 2.49 }, { id: 7, name: "Basil", price: 1.49 }]
-    },
-  ];
-
-  orderStore.addOrder(dlongOrderItems);
-}
-const addData = () => {
   const exampleOrderItem1: OrderItem = {
     count: 2,
     price_sum: 25.99,
@@ -149,9 +81,9 @@ const addData = () => {
 </style>
 
 <style>
-  h2 {
-    text-decoration: underline;
-    line-height: 1.5;
-    text-align: center;
-  }
+h2 {
+  text-decoration: underline;
+  line-height: 1.5;
+  text-align: center;
+}
 </style>
