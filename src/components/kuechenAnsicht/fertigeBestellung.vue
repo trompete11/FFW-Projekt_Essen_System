4d5cde01cd5fbce8be44af4854ec445a03b6230d
Burak Koc <152020621+firefist3d2y@users.noreply.github.com>
2024-02-29 21:51:24 +0100

    Added comments. Tried to fix removal of gone orders.

--- src/components/kuechenAnsicht/fertigeBestellung.vue
+++ src/components/kuechenAnsicht/fertigeBestellung.vue
@@ -1,3 +1,5 @@
+<!-- Author: Emre Burak Koc -->
+<!-- Component for displaying done orders in sidebar to uncheck them -->
 <template>
   <div v-if="!orderStore.getOrders[props.orderId - 1].time_gone">
     <input type="checkbox" @change="setDone(props.orderId - 1, false)" checked />
@@ -13,10 +15,11 @@ import { ref, reactive, watchEffect } from 'vue'
 const props = defineProps<{
   orderId: number
 }>()
-
+  
 // getting state of done orders 
-const orderStore = useOrderStore()
+const orderStore = ref(useOrderStore())
 const setDone = orderStore.doneOrder
+//const gone = ref(orderStore.getOrders[props.orderId - 1].time_gone);
 </script>
 
 <style scoped></style>
